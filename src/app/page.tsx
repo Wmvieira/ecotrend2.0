e9b48450a301a50ff49351e1bca19a4af68404e5
Wictor Vieira <wictormvieira@gmail.com>
2024-06-11 20:46:20 -0300

    redirect to hero page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
 export default function Home() {
+  const router = useRouter();
+
+  router.push("/hero");
+
   return <main className=""></main>;
 }
