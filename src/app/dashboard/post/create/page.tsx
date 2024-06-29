9ec3cf2cfa1ea964e351e53a75427bdc6e723849
Wictor Vieira <wictormvieira@gmail.com>
2024-06-29 18:19:46 -0300

    chore: Update refetch interval for comments and tips

--- src/app/dashboard/post/create/page.tsx
+++ src/app/dashboard/post/create/page.tsx
@@ -5,20 +5,35 @@ import { Textarea } from "~/components/ui/textarea";
 import { Button } from "~/components/ui/button";
 import { useRouter } from "next/navigation";
 import { api } from "~/trpc/react";
+import Spinner from "~/components/ui/spinner";
+import { FaPaperPlane } from "react-icons/fa6";
+import { toast } from "~/components/ui/use-toast";
+
+const MAX_TEXTAREA_LENGTH = 3500;
 
 const TipFormPage: React.FC = () => {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
   const router = useRouter();
 
-  const { mutate: createTip } = api.tip.createPost.useMutation({
-    onSuccess: (rate) => {
+  const { mutate: createTip, isPending } = api.tip.createPost.useMutation({
+    onSuccess: () => {
+      toast({ content: "Postagem criada com sucesso" });
       void router.push("/dashboard");
     },
   });
 
+  const handleAddDescription = (
+    event: React.ChangeEvent<HTMLTextAreaElement>,
+  ) => {
+    if (description.length < MAX_TEXTAREA_LENGTH) {
+      setDescription(event.target.value);
+    }
+  };
+
   const handleSubmit = (event: React.FormEvent) => {
     event.preventDefault();
+    if (!title || !description) return;
     createTip({
       title,
       content: description,
@@ -26,25 +41,39 @@ const TipFormPage: React.FC = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div className="flex flex-col items-center justify-center md:px-14">
-        <div className="mt-4 w-full md:w-1/2">
-          <Input
-            placeholder="Titulo do post"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-          />
-        </div>
-        <div className="mt-4 w-full md:w-1/2">
-          <Textarea
-            placeholder="Digite o conteudo da postagem"
-            value={description}
-            onChange={(e) => setDescription(e.target.value)}
-          />
-        </div>
-        <div className="mt-4 flex w-full justify-end md:w-1/2">
-          <Button type="submit">Enviar</Button>
-        </div>
+    <form
+      onSubmit={handleSubmit}
+      className="flex flex-col items-center justify-center gap-2 p-2 md:px-14"
+    >
+      <div className="w-full">
+        <label className="text-lg font-bold">Titulo</label>
+        <Input
+          placeholder="Titulo do post"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+      </div>
+      <div className="w-full">
+        <label className="text-lg font-bold">Conteudo</label>
+        <Textarea
+          placeholder="Digite o conteudo da postagem"
+          value={description}
+          onChange={handleAddDescription}
+          onKeyDown={(e) =>
+            e.key == "Backspace" &&
+            description.length == MAX_TEXTAREA_LENGTH &&
+            setDescription(description.slice(0, -1))
+          }
+          rows={15}
+        />
+        <span className="text-sm text-gray-500">
+          {MAX_TEXTAREA_LENGTH - description.length} caracteres restantes
+        </span>
+      </div>
+      <div className="flex w-full justify-end">
+        <Button onClick={handleSubmit}>
+          {isPending ? <Spinner /> : <FaPaperPlane />}
+        </Button>
       </div>
     </form>
   );
